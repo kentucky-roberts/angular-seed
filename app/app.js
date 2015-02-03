bf0ede88f875734a70bf5d12eccf9a7a53edbf8a
kentucky-roberts <john@heels.com>
2015-02-03 11:00:23 -0500

    Added pages; home, about, contact and sandbox

--- app/app.js
+++ app/app.js
@@ -1,13 +1,39 @@
-angular
-    .module('app', ['ngRoute'])
+var app = angular.module("app", ["ngRoute"]);
 
-    .config(['$routeProvider', function($routeProvider) {
-      $routeProvider.when('/', {
-        templateUrl: 'templates/home.html',
-        controller: 'HomeCtrl'
-      });
-    }])
+app.config(function($routeProvider) {
+    $routeProvider
+      
+      .when('/',
+      {
+        templateUrl: "templates/home.html",
+        controller: "AppCtrl"
+      })
+      
+      .when('/about',
+      {
+        templateUrl: "templates/about.html",
+        controller: "AppCtrl"
+      })
+      
+      .when('/contact',
+      {
+        templateUrl: "templates/contact.html",
+        controller: "AppCtrl"
+      })
+      
+      .when('/sandbox', 
+      {
+        templateUrl: "templates/sandbox.html"
+      })
 
-    .controller('HomeCtrl', [function() {
+      .otherwise({
+        template: "This doesn't exist!"
+      })
+});
 
-    }]);
+app.controller("AppCtrl", function($scope) {
+
+    $scope.model = {
+        message: "This is my app!!!"
+    }
+});
