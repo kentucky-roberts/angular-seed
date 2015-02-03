b48b6596cb5cbed089840bc573bbdd939620d95a
kentucky-roberts <john@heels.com>
2015-02-02 19:51:00 -0500

    Post bower and npm update Commit

--- app/app.js
+++ app/app.js
@@ -1,12 +1,13 @@
-'use strict';
+angular
+    .module('app', ['ngRoute'])
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+    .config(['$routeProvider', function($routeProvider) {
+      $routeProvider.when('/', {
+        templateUrl: 'templates/home.html',
+        controller: 'HomeCtrl'
+      });
+    }])
+
+    .controller('HomeCtrl', [function() {
+
+    }]);
